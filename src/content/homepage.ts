58a6915bfa7c4c3d4db82794727c7ab2eec908bd
greensoap <william.sjokvist@gmail.com>
2023-03-21 14:57:43 +0100

    Project cards are now reading from mdx

--- src/content/homepage.ts
+++ src/content/homepage.ts
@@ -2,16 +2,16 @@ export const homepage = [
   {
     title: "Blazingly Fast",
     content: "The reason why I chose Astro is because it's blazingly fast. It's also a component-based framework, so you can easily reuse your components.",
-    icon: "charm:rocket",
+    imgSrc: "images/portrait-beagle-unsplash.jpg",
   },
   {
     title: "Easy to use",
     content: "Need to use this template? Just clone it and start writing your content. No complicated setup, no complicated design.",
-    icon: "ph:code-bold",
+    imgSrc: "images/jules-beagle-unsplash.jpg",
   },
   {
     title: "Free and Open Source",
     content: "Found some issues or want to contribute? Feel free to open an issue or pull request on GitHub.",
-    icon: "charm:plant-pot",
+    imgSrc: "images/portrait-beagle-unsplash.jpg",
   },
 ];
