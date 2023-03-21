7f239b27981e8f296f18ed79011cae6efd623a4a
greensoap <william.sjokvist@gmail.com>
2023-03-21 18:09:04 +0100

    Adjusted styling

--- astro.config.ts
+++ astro.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
         {
           default: "@layouts/Layout.astro",
           "pages/blog/**/*.mdx": "@layouts/BlogLayout.astro",
+          "pages/projects/**/*.mdx": "@layouts/ProjectLayout.astro",
         },
       ],
       codeTitle,
